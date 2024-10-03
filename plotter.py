74f2417cc7740b825bfb2b5936a23fe972ecbd66
AngeloGalav <agalavottib@gmail.com>
2024-10-03 17:25:48 +0200

    Plotter fix

--- plotter.py
+++ plotter.py
@@ -17,8 +17,10 @@ def display_image(image):
 
 def plot_metrics(values, metric: str):
     train_val, test_val = values
-    plt.plot(train_val, label = metric)
-    plt.plot(test_val, label='val_' + metric)
+    train_accs_cpu = [t.cpu() for t in train_val if t.is_cuda]
+    test_accs_cpu = [t.cpu() for t in test_val if t.is_cuda]
+    plt.plot(train_accs_cpu, label = metric)
+    plt.plot(test_accs_cpu, label='val_' + metric)
     plt.xlabel('Epochs')
     plt.ylabel(metric)
     plt.legend()
