05f5343846d1a29f4627a31ab937f1733b1f3030
AngeloGalav <agalavottib@gmail.com>
2024-10-03 15:02:24 +0200

    Add plot_metrics function

--- plotter.py
+++ plotter.py
@@ -14,3 +14,12 @@ def plot_losses(losses):
 # stupid function but is more readable
 def display_image(image):
     plt.imshow(image);
+
+def plot_metrics(values, metric: str):
+    train_val, test_val = values
+    plt.plot(train_val, label = metric)
+    plt.plot(test_val, label='val_' + metric)
+    plt.xlabel('Epochs')
+    plt.ylabel(metric)
+    plt.legend()
+    plt.show()
