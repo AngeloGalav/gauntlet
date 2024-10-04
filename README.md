014b1e6614a665520191cd4062a64b58461e337f
AngeloGalav <agalavottib@gmail.com>
2024-10-04 20:33:08 +0200

    Finetuned new model (ResNet50), add LIME and GradCam
    Gradcam and LIME do not produce good results it seems. We should investigate why.
    The new resnet50 also acheives 83% accuracy, and we can extend it further with the new finetuning strategy i have in mind (explained in the todos)

--- README.md
+++ README.md
@@ -10,9 +10,18 @@ cant commit those since they are too big...
 Plz keep the todo list updated!
 - [x] Create some models
     - [ ] standard CNN
-    - [ ] Fine-tuned resnet
-    - [ ] Implement early stopping
-- [ ] test models
+    - [x] Fine-tuned resnet
+    - [ ] Implement early stopping?
+- [x] test models
 - [ ] do Xai stuff
-    - [ ] find a method for XAI (LIME?)
+    - [x] find a method for XAI (LIME?)
+    - [ ] fix LIME (batch_predict), test if predictions are consistent with the other ()
+    - [ ]
+- [ ] Add more graphs?
+- [ ] better Finetuning of resnet50 (fine tune feature extractor after finetuning classifier)
 - [ ] solve all the other code TODOs (ctrl+shift+f TODO)
+
+## Useful links
+https://coderzcolumn.com/tutorials/artificial-intelligence/lime-explain-keras-image-classification-network-predictions
+
+https://www.kaggle.com/code/hamedaraab/classification-of-real-and-ai-generated-images#Utilities
--- xai.py
+++ xai.py
@@ -1,1 +1,98 @@
 # scripts for xai stuff
+import matplotlib.pyplot as plt
+from PIL import Image
+import torch.nn as nn
+import numpy as np
+import os, json
+
+import torch
+import torch.nn.functional as F
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pytorch_grad_cam import AblationCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+
+device = 'cpu'
+
+# LIME predict function, returns class probabilities
+def batch_predict(images, model):
+    # Convert list of numpy arrays to tensor
+    batch = torch.stack([torch.tensor(i).permute(2, 0, 1) for i in images]).to(device)
+    logits = model(batch)
+    probs = torch.sigmoid(logits)
+    return probs.detach().cpu().numpy()
+
+
+def explain_lime_single_image(dataloader, model):
+    for images, _ in dataloader:
+        # Select the first image in the batch
+        image = images[0]  # Tensor format
+
+        # place data in correct channels
+        numpy_image = image.permute(1, 2, 0).cpu().numpy()
+
+        # Initialize LIME Image Explainer
+        explainer = lime_image.LimeImageExplainer()
+        explanation = explainer.explain_instance(
+            numpy_image,  # Image as numpy array
+            lambda x: batch_predict(x, model),  # Prediction function
+        )
+
+        # Get the image and mask for the top predicted class
+        img, mask = explanation.get_image_and_mask(
+            explanation.top_labels[0],
+            positive_only=False,
+            hide_rest=True
+        )
+
+        fig = plt.figure(figsize=(15,5))
+
+        ax = fig.add_subplot(141)
+        ax.imshow(numpy_image, cmap="gray");
+        ax.set_title("Original Image")
+        ax = fig.add_subplot(142)
+        ax.imshow(img);
+        ax.set_title("Image")
+        ax = fig.add_subplot(143)
+        ax.imshow(mask);
+        ax.set_title("Mask")
+        ax = fig.add_subplot(144)
+        ax.imshow(mark_boundaries(img, mask, color=(0,1,0)));
+        ax.set_title("Image+Mask Combined");
+
+        break  # Only run for the first batch
+
+def explain_gradcam_single_image(dataloader, model, target_layers):
+    model.model.eval()
+    for images, _ in dataloader:
+
+        image = images[0]
+        numpy_image = image.permute(1, 2, 0).cpu().numpy()
+        am = AblationCAM(model=model, target_layers=target_layers)
+
+        def image_mapper(image):
+            grayscale_am = am(
+                input_tensor=image.unsqueeze(0),
+                targets=[ClassifierOutputTarget(0)],
+            )[0]
+
+            return show_cam_on_image(
+                image.permute(1, 2, 0).numpy(),
+                grayscale_am,
+                use_rgb=True,
+            )
+
+        plt.imshow(image_mapper(image))
+
+        break  # Only run for the first batch
