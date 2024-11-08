9a9356712a0d1693924f4052fb9fb8ecaa23d9de
AngeloGalav <agalavottib@gmail.com>
2024-11-08 01:22:25 +0100

    Add logo to navbar

--- webapp/frontend/src/components/NavBar.jsx
+++ webapp/frontend/src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import "./NavBar.css"; // Import the CSS file
+import Logo from "../logo.png" 
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo gloock-regular">Gauntlet xAI</div>
+      <div class="navbar-logo gloock-regular">
+          <img src={Logo} className="logo" />
+          Gauntlet xAI
+      </div>
       <div className="navbar-button">
         About
       </div>
